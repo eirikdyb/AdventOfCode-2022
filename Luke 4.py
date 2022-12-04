cb7789faa9d51d57a4d50cc5c990b54043fdef79
eirikdyb <107116260+eirikdyb@users.noreply.github.com>
2022-12-04 09:22:05 +0100

    Update Luke 4.py

--- Luke 4.py
+++ Luke 4.py
@@ -9,29 +9,25 @@ with open("input.txt",'r') as content:
 
 def check_for_subset(intervalls_1, intervalls_2):
     overlap = 0
-    i = 0
-    
-    while i <= (len(intervalls_1) - 1):
+    for i in range(0, len(ranges_1) - 1, 2):
         if intervalls_1[i+1] >= intervalls_2[i+1] and intervalls_1[i] <= intervalls_2[i]:
             overlap += 1
         elif intervalls_2[i+1] >= intervalls_1[i+1] and intervalls_2[i] <= intervalls_1[i]:
             overlap += 1
-        i += 2
         
     return overlap
 
 def check_for_cmn_elmnt(intervalls_1, intervalls_2):
-    i = 0
     overlap = 0
-    
-    while i <= (len(ranges_1) - 1):
+    for i in range(0, len(ranges_1) - 1, 2):
         if intervalls_2[i] <= intervalls_1[i] <= intervalls_2[i+1] or intervalls_2[i] <= intervalls_1[i+1] <= intervalls_2[i+1]:
             overlap += 1
         elif intervalls_1[i] <= intervalls_2[i] <= intervalls_1[i+1] or intervalls_1[i] <= intervalls_2[i+1] <= intervalls_1[i+1]:
             overlap += 1
-        i += 2
         
     return overlap
     
 print(check_for_subset(ranges_1, ranges_2))
 print(check_for_cmn_elmnt(ranges_1, ranges_2))
+
+    
