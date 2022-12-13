89035ffdccdbf651d93f9838645f5c35576a8fd2
Gjestl <106059641+Gjestl@users.noreply.github.com>
2022-12-13 16:07:12 +0100

    Update luke 13.py
    Changed to recursion (using True/False)

--- luke 13.py
+++ luke 13.py
@@ -16,46 +16,38 @@ def compare_list_and_list(A, B):
     max_length = max(len(A), len(B))
     for k in range(max_length):
         if k > len(B) - 1:
-            return 0
+            return False
         elif k > len(A) - 1:
-            return 1
+            return True
         if isinstance(A[k], int) and isinstance(B[k], int):
             if A[k]> B[k]:
-                return 0
+                return False
             elif A[k] < B[k]:
-                return 1
+                return True
         else:
             if isinstance(A[k], list) and isinstance(B[k], int):
-                if compare_list_and_list(A[k], [B[k]]) == 0:
-                    return 0
-                elif compare_list_and_list(A[k], [B[k]]) == 1:
-                    return 1
+                return compare_list_and_list(A[k], [B[k]])
             elif isinstance(A[k], int) and isinstance(B[k], list):
-                if compare_list_and_list([A[k]], B[k]) == 0:
-                    return 0
-                elif compare_list_and_list([A[k]], B[k]) == 1:
-                    return 1
-            elif isinstance(A[k], list) and isinstance(B[k], list):
-                if compare_list_and_list(A[k], B[k]) == 0:
-                    return 0
-                elif compare_list_and_list(A[k], B[k]) == 1:
-                    return 1
+                return compare_list_and_list([A[k]], B[k])
+            elif isinstance(A[k], list) and isinstance(B[k], list) and compare_list_and_list(A[k], B[k]) in (True, False):
+                return compare_list_and_list(A[k], B[k])
 
 input = readfile()
 sum_of_pairs = 0
 for i in range(0, len(input), 2):
-    if compare_list_and_list(input[i], input[i+1]) == 1:
+    if compare_list_and_list(input[i], input[i+1]):
         sum_of_pairs += int(i/2+1)
+
 print(sum_of_pairs)
 
 sorted_packets = [[[2]],[[6]]]
 for packet in input:
     for j in range(len(sorted_packets)):
         counter = 0
-        if compare_list_and_list(packet, sorted_packets[j]) == 1:
+        if compare_list_and_list(packet, sorted_packets[j]):
             sorted_packets.insert(j,packet)
             break
-        elif compare_list_and_list(packet, sorted_packets[j]) == 0:
+        else:
             counter += 1
     if counter > 0:
         sorted_packets.append(packet)
